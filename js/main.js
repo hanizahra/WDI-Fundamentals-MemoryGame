ffadbc768eed6fdab528885af5e030d6ee0a542b
Hani Zahra <hanizahra@Hanis-MacBook-Pro-7.local>
2017-12-21 15:32:44 -0500

    unit 10 assignment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 //console.log("Up and running!");
 //console.log("User flipped " + cardOne);
-var cards = ["queen", "queen", "king", 'king'];
+var cards = [
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
+
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 console.log("You found a match!");
@@ -9,12 +36,17 @@ console.log("You found a match!");
 alert("Sorry, try again.");
 }
 }
-var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID])
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
     if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
   };
+
 flipCard(0);
 flipCard(2);
+
