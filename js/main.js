c7a722ca5e000b346b2fcdad4666c6c0cfd9b946
Hani Zahra <hanizahra@Hanis-MacBook-Pro-7.local>
2017-12-21 11:31:39 -0500

    unit 9 assignment

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@
 //console.log("User flipped " + cardOne);
 var cards = ["queen", "queen", "king", 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} 
-	else {
-		alert("Sorry, try again.")
-	}
+console.log("You found a match!");
+} else {
+alert("Sorry, try again.");
 }
+}
+var flipCard = function (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID])
+    if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+  };
+flipCard(0);
+flipCard(2);
