1380b797b3a9ac9cc98de4f5b3ced6d74499cea0
Hani Zahra <hanizahra@Hanis-MacBook-Pro-7.local>
2017-12-22 01:10:56 -0500

    unit 11 assignment - Finished!

--- js/main.js
+++ js/main.js
@@ -31,22 +31,41 @@ var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
 alert("Sorry, try again.");
 }
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
   };
 
-flipCard(0);
-flipCard(2);
+ var createBoard = function () {
+  	for (var i = 0; i < cards.length; i++) {
+  		var cardElement = document.createElement('img');
+  		cardElement.setAttribute('src', 'images/back.png');
+  		cardElement.setAttribute('data-id', i);
+  		cardElement.addEventListener('click', flipCard);
+  		document.getElementById('game-board').appendChild(cardElement);
+  	}
+  };
+
+  createBoard();
+
+  document.getElementById('button').onclick=function () {
+  		location.reload();
+  };
+
+  
+
+
 
